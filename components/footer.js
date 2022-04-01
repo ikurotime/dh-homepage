557d16ef48dc012004212ed2df69436ba9fa9ba7
David Huertas <davidhuertasortiz@gmail.com>
2022-04-01 11:26:36 +0200

    Add footer link

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,7 @@ import { Box } from '@chakra-ui/react'
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
-      &copy; {new Date().getFullYear()} David Huertas.
+      &copy; {new Date().getFullYear()} David Huertas. Original design inspired by <a href="https://www.youtube.com/channel/UC7yZ6keOGsvERMp2HaEbbXQ">devaslife</a>
     </Box>
   )
 }
